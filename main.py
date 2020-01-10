c1307414a311215bc7ec144c1f5315e2dfd921af
mehranasghari <mhr.as1317@gmail.com>
2020-01-10 23:31:20 +0330

    comment

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import simulation
 from SystemClass import System
 from StatisticalData import StatisticalData
 
-max_number_of_tasks = 50_000_000
-number_of_warm_up_task = 5_000
+max_number_of_tasks = 50_000_000  # maximum number of tasks
+number_of_warm_up_task = 5_000  # after this number of tasks we begin to collect the statistics
 
-input_data = get_data.get(input("input file address: "))
-task_generator = get_initial_data_generator.get_task_generator(input_data)
-system = System(input_data)
+input_data = get_data.get(input("input file address: "))  # getting data from an input file
+task_generator = get_initial_data_generator.get_task_generator(input_data)  # make a task generator
+system = System(input_data)  # make a main system
 
 simulation_result = simulation.simulate(number_of_warm_up_task, max_number_of_tasks, task_generator, system,
                                         StatisticalData(input_data))
